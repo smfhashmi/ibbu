214b40bde28941bfd76b3ad31e4a38a67a5fb237
hashmi mohammed <hashmi.smf@gmail.com>
2026-04-07 14:10:24 -0500

    added js

--- public/index.js
+++ public/index.js
@@ -1,5 +1,4 @@
-var k = document.getElementById("name")?.textContent
-console.log(k)
+console.log(document.getElementById("name")?.textContent)
 document.getElementById("name").textContent = "Apki pehchan kya hai?";
 document.getElementById("name").style.color = "blue";
 document.getElementById("name").style.fontSize = "50px";
