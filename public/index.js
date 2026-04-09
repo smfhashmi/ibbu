a23e85c51578361bb525c1d8da3b88da41bcad6c
syedibrahim1229 <syedibrahim2907@gmail.com>
2026-04-09 12:47:33 -0500

    Removed Hindi Text

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 var k = document.getElementById("name")?.textContent
 console.log(k)
-document.getElementById("name").textContent = "Apki pehchan kya hai?";
+document.getElementById("name").textContent = "...?";
 document.getElementById("name").style.color = "blue";
 document.getElementById("name").style.fontSize = "50px";
