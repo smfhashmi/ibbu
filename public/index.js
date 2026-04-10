e0e6614b79962cd90e8cefc21010830b8fb83ea0
hashmi mohammed <hashmi.smf@gmail.com>
2026-04-10 10:18:10 -0500

    created js form and submitted

--- public/index.js
+++ public/index.js
@@ -1,5 +1,12 @@
-var k = document.getElementById("name")?.textContent
-console.log(k)
-document.getElementById("name").textContent = "...?";
-document.getElementById("name").style.color = "blue";
-document.getElementById("name").style.fontSize = "50px";
+document.getElementById('userForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  const name = document.getElementById('name').value;
+  const age = document.getElementById('age').value;
+  const mobile = document.getElementById('mobile').value;
+
+  document.getElementById('details').innerHTML = `
+    <h2>User Details</h2>
+    <p>Name: ${name}</p>
+    <p>Age: ${age}</p>
+    <p>Mobile: ${mobile}</p>`;
+})
