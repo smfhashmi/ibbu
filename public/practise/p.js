ad507eff51b8025e9a85614b40b38b2ca1ff40cd
syedibrahim1229 <syedibrahim2907@gmail.com>
2026-04-11 07:11:32 -0500

    results
    practised and results known with tilde used in it.

--- public/practise/p.js
+++ public/practise/p.js
@@ -1,12 +1,16 @@
-document.getElementById('class details').addEventListener('submit', function (e) {
+document.getElementById('details').addEventListener('submit', function (e) {
  e.preventDefault();
 const name = document.getElementById('name').value;
 const subject = document.getElementById('subject').value;
-const hours per week = document.getElementById('hours per week').value;
+const hoursperweek = document.getElementById('hours per week').value;
 
-document.getElementById('subject details').innerHTML = '
-<h3> subject details</h3>
+document.getElementById('results').innerHTML = `
+<h3> subject details </h3>
 <p>name: ${name}</p>
 <p>subject: ${subject}</p>
-<p>hours per week: ${hours per week}</p>';
+<p>hoursperweek: ${hoursperweek}</p>`;
 })
+
+
+
+document.getElementById('details').add
