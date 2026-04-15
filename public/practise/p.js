8b0f8ab373f5b67864a88c3804aa6b001f2d045f
syedibrahim1229 <syedibrahim2907@gmail.com>
2026-04-15 12:58:14 -0500

    radio learned
    last night forgot to push and learned radio only in html

--- public/practise/p.js
+++ public/practise/p.js
@@ -2,15 +2,15 @@ document.getElementById('details').addEventListener('submit', function (e) {
  e.preventDefault();
 const name = document.getElementById('name').value;
 const subject = document.getElementById('subject').value;
-const hoursperweek = document.getElementById('hours per week').value;
+const hoursperweek = document.getElementById('hoursperweek').value;
+const gender = document.getElementById('gender').value;
+const radio = document.getElementById('HighestQualification').value;
 
 document.getElementById('results').innerHTML = `
 <h3> subject details </h3>
 <p>name: ${name}</p>
 <p>subject: ${subject}</p>
-<p>hoursperweek: ${hoursperweek}</p>`;
+<p>hoursperweek: ${hoursperweek}</p>
+<p>gender: ${gender}</p>
+<p>radio: ${HighestQualification}`;
 })
-
-
-
-document.getElementById('details').add
